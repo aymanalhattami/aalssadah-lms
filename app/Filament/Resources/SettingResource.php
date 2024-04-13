2f5fd42b3357296f07098f8f0983b069f028c15e
ala <ala.alssadah@gmail.com>
2024-04-13 17:38:57 +0300

    feat: create relationships between tables add filament setting resourse edit filament setting resourse

---
+++ app/Filament/Resources/SettingResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SettingResource\Pages;
+use App\Filament\Resources\SettingResource\RelationManagers;
+use App\Models\Setting;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SettingResource extends Resource
+{
+    protected static ?string $model = Setting::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('site_name')
+                    ->required()
+                    ->maxLength(100),
+                Forms\Components\Textarea::make('goal')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('vision')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('logo')
+//                    ->disk('local')
+                    ->directory('logo-attachments')
+//                    ->visibility('private')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('site_name'),
+                Tables\Columns\TextColumn::make('goal'),
+                Tables\Columns\TextColumn::make('vision'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSettings::route('/'),
+            'edit' => Pages\EditSetting::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Course extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'name',
+        'thumbnail',
+        'status'
+    ];
+
+    public function users() :BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function lessons() :HasMany
+    {
+        return $this->HasMany(Lesson::class);
+    }
 }
