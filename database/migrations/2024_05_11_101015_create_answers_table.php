97f49f510b50085652d69e057853e7f2879556f0
ala <ala.alssadah@gmail.com>
2024-05-12 20:37:50 +0300

    feat: add answer model add answer migration edit exam relation edit course relation edit migration exam and question

--- database/migrations/2024_05_11_101015_create_answers_table.php
+++ database/migrations/2024_05_11_101015_create_answers_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('questions', function (Blueprint $table) {
+        Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('exam_id')->nullable()->constrained();
-            $table->string('question')->nullable();
-            $table->integer('status')->default(1);
+            $table->foreignId('question_id')->nullable()->constrained();
+            $table->string('answer')->nullable();
+            $table->boolean('is_correct')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('answers');
     }
 };
